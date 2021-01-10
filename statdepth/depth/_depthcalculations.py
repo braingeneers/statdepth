6f009b61801819305d2e44c942622c133a077ba0
Julian Lehrer <jmlehrer@ucsc.edu>
2021-01-10 11:26:37 -0800

    Added BandDepth initial class, renamed files
    Working on making "dummy" class for banddepth to have some simple
    attributes like those in the paper

--- statdepth/depth/_depthcalculations.py
+++ statdepth/depth/_depthcalculations.py
@@ -19,7 +19,41 @@ from ._containment import _simplex_containment
 from ._containment import _select_containment
 from ._containment import _is_valid_containment
 
-def banddepth(data: List[pd.DataFrame], J=2, containment='r2', relax=False, deep_check=False) -> Union[pd.Series, pd.DataFrame]:
+class _BandDepthSeries(pd.Series):
+
+    def __init__(self, depths):
+        super().__init__(data=depths)
+
+        self._depths = depths
+        self._ordered_depths_ = None
+        self._deepest_curve_ = None
+
+    def ordered(self, ascending=False):
+        if self._ordered_depths_ is None:
+            self._ordered_depths_ =  self._depths.sort_values(ascending=ascending)
+        return self._ordered_depths_
+
+    def deepest(self):
+        if self._ordered_depths_ is not None or (self._ordered_depths_ is None and self._deepest_curve_ is None):
+            sorted_depths = self._depths.sort_values(ascending=False)
+            self._deepest_curve_ = pd.Series(index=[sorted_depths.index[0]], data=[sorted_depths[0]])
+        return self._deepest_curve_
+    
+class _BandDepthDataFrame(pd.DataFrame):
+    def __init__(self):
+        pass 
+
+# def BandDepth(data: List[pd.DataFrame], K=None, J=2, containment='r2', relax=False, deep_check=False) -> Union[_BandDepthSeries, _BandDepthDataFrame]:
+#     if K is not None:
+#         data = _samplebanddepth(data=data, K=K, J=J, containment=containment, relax=relax, deep_check=deep_check)
+#     else:
+#         data = _banddepth(data=data, J=J, containment=containment, relax=relax, deep_check=deep_check)
+
+#     if isinstance(data, pd.DataFrame):
+#         return _BandDepthDataFrame(data)
+#     else:
+
+def _banddepth(data: List[pd.DataFrame], J=2, containment='r2', relax=False, deep_check=False) -> Union[pd.Series, pd.DataFrame]:
     """
     Calculate the band depth for a set of functional curves.
 
@@ -45,7 +79,7 @@ def banddepth(data: List[pd.DataFrame], J=2, containment='r2', relax=False, deep
 
     Returns:
     ----------
-    pd.Series: Depth values for each function.
+    pd.Series, pd.DataFrame: Depth values for each function.
     """
 
     # Handle common errors
@@ -72,7 +106,7 @@ def banddepth(data: List[pd.DataFrame], J=2, containment='r2', relax=False, deep
     return None
 
 
-def samplebanddepth(data: List[pd.DataFrame], K: int, J=2, containment='r2', relax=False, deep_check=False) -> Union[pd.Series, pd.DataFrame]:
+def _samplebanddepth(data: List[pd.DataFrame], K: int, J=2, containment='r2', relax=False, deep_check=False) -> Union[pd.Series, pd.DataFrame]:
     """
     Calculate the sample band depth for a set of functional curves.
 

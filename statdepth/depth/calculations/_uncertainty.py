f998073028ecf4dc3f705a1e9ddbc672cd44cba3
Julian Lehrer <jmlehrer@ucsc.edu>
2021-05-05 20:26:27 -0700

    Added simplified poisson calculation -- much faster

--- statdepth/depth/calculations/_uncertainty.py
+++ statdepth/depth/calculations/_uncertainty.py
@@ -20,19 +20,26 @@ __all__ = ['probabilistic_normal_depth', 'probabilistic_poisson_depth']
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 # Poisson
 def _poisson_containment(lambda_i: float, lambda_f: float, lambda_j: float, lim: int, tol: float, quiet=False) -> float:
+    '''Explicitely calculates Pr(f_i <= f <= f_j) where f_i is the Poisson distribution with mean lambda_i, f is the 
+    Poisson distribution with mean lambda_f, and f_j is the Poisson distribution with lambda_j'''
     s = 0
     
     for z in range(1, lim):
         ls = sum(poisson.pmf(k=l, mu=lambda_i) for l in range(z))
         ks = sum(poisson.pmf(k=k, mu=lambda_j) for k in range(z, lim))
         c = ls * ks * poisson.pmf(k=z, mu=lambda_f)
-        
-        if c <= tol:
-            break
 
         s += c
 
     return s
+
+def _poisson_containment_simplified(lambda_i, lambda_f, lambda_j, lim=100, quiet=False) -> float:
+    '''Calculates Pr(f_i <= f <= f_j) like above, but uses the simplification found by Wolfram in order to speed up computation.
+    This method takes about half the time. '''
+    return exp(-lambda_f) * sum(
+        (np.power(lambda_f, z)*(gamma(z)-gammainc(z, lambda_j))*gammainc(1+z, lambda_i))/\
+        (factorial(z)*gamma(z)*gamma(1+z)) for z in range(1, lim)
+    )
 
 def _poisson_depth(df: pd.DataFrame, curr: int, lim: int, tol: float, to_compute=None):
     n, p = df.shape
@@ -45,7 +52,7 @@ def _poisson_depth(df: pd.DataFrame, curr: int, lim: int, tol: float, to_compute
     for sequence in subseq:
         i, j = sequence
         S_nj += sum(
-            _poisson_containment(
+            _poisson_containment_simplified(
                 df.loc[k, i], 
                 df.loc[k, curr], 
                 df.loc[k, j],
@@ -67,7 +74,7 @@ def probabilistic_poisson_depth(df: pd.DataFrame, to_compute=None, lim=1000, tol
 
 # # # # # # # # # # # # # # # # # # # # # # # #
 # Binomial
-def _binom_containment(params: list, lim=1000) -> float:
+def _binom_containment(params: list, lim=3000) -> float:
     n_i, p_i, n, p, n_j, p_j = params
     
     s = 0
